231cd551fe82f8a170c2522331eff09e8725ed68
= <vinodseervi0124@gmail.com>
2023-07-19 10:52:17 +0530

    .

--- e-commerce/src/test/java/com/ecommerce/ECommerceApplication.java
+++ e-commerce/src/test/java/com/ecommerce/ECommerceApplication.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JwtYoutubeApplicationTests {
+class ECommerceApplication {
 
     @Test
     void contextLoads() {
